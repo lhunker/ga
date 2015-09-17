9274187a4cb8ac13fd2791c8623cfd29ebdfb6fe
lhunker <lukas.hunker@gmail.com>
2015-09-17 16:45:42 -0400

    Adding a main file

---
+++ src/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lhunker on 9/17/15.
+ * The main file for the genetic algorithm assignment
+ */
+
+//This file will be called from the command line
+//Kick off any processing here
