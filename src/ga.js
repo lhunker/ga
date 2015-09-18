ebb72d6684980d36fd4520910d396d229718711a
Brett Ammeson <ammesonb@gmail.com>
2015-09-17 23:48:31 -0400

    Adds order one crossover function

--- src/ga.js
+++ src/ga.js
@@ -14,8 +14,7 @@
  * @constructor
  */
 function GA(list, fitness, permeate, include){
-  this.list = list;
-  this.fitness = fitness;
+  this.list = list; this.fitness = fitness;
   this.permutate = permeate;
   this.include = include;
 }
@@ -50,5 +49,38 @@ function reconstitute(order, included){
   return [];
 }
 
+function orderOneCrossover(arr1, arr2, switched_arrays) {
+    console.log("Array 1: " + arr1.join(", "));
+    console.log("Array 2: " + arr2.join(", "));
+    // Select a random starting point at least one away from the end
+    var start = Math.floor(Math.random() * (arr1.length - 1));
+    // Select a length to keep
+    var len = Math.floor(Math.random() * (arr1.length - start));
+    if (len == 0) {
+        len = 1;
+    }
+    console.log("Start: " + start + ", Length: " + len);
+    // Create child1 from that range
+    var child1 = arr1.slice(start, start + len);
+    console.log("Child 1: " + child1.join(", "));
+    var parent2 = [];
+    var i;
+    // Create a copy of arr2
+    for (i = 0; i < arr2.length; i++) parent2.push(arr2[i]);
+    // and then remove values from the new child
+    for (i = 0; i < child1.length; i++) parent2.splice(parent2.indexOf(child1[i]), 1);
+    console.log("Updated array 2: " + parent2.join(", "));
+    // Copy values at right edge of array, - 1 because indexes aren't inclusive
+    var rightCount = arr2.length - start - len;
+    var leftCount = parent2.length - rightCount;
+    for (i = rightCount; i > 0; i--) child1.push(parent2[parent2.length - i]);
+    // Copy remaining values to left edge
+    for (i = leftCount - 1; i >= 0; i--) child1.splice(0, 0, parent2[i]);
+    console.log("Completed child 1: " + child1.join(", "));
+    var child2;
+    if (!switched_arrays) child2 = orderOneCrossover(arr2, arr1, true);
+    else return child1;
+    return {child1: child1, child2: child2};
+}
 
 module.exports = GA;
