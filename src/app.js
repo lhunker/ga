96981f0569a62feb2002c2dfce765eacbd73adba
Brett Ammeson <ammesonb@gmail.com>
2015-09-23 14:34:33 -0400

    Fixes scoping on packer fitness function

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@
 var GA = require('./ga');
 var Packer = require('./Packer');
 var packer = new Packer();
-packer.loadFile('./numbers', function() {
+packer.loadFile('numbers', function() {
     /*var fitness = function(list){
       var sum = 0;
         list.forEach(function (l, i) {
@@ -19,7 +19,7 @@ packer.loadFile('./numbers', function() {
         return sum;
     };*/
     //var test = new GA([1,5,7,8,3,9,2], fitness, true, true);
-    var test = new GA(packer.numbers, packer.fitness, true, true);
+    var test = new GA(packer.numbers, packer.fitness.bind(packer), true, true);
 
     console.info('Solution = ' + test.run(10, 5));
 });
